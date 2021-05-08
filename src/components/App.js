1c15f4ab6d17d4446f5a61dfe69d3fd9ae9630f8
Daniel Olshansky <olshansky.daniel@gmail.com>
2021-05-08 08:17:06 -0700

    Added an ugly search bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import SearchBar from "./SearchBar";
 
 const App = () => {
-  return <div>Test</div>;
+  return (
+    <div>
+      Test
+      <SearchBar></SearchBar>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <input type="text"></input>
+        </form>
+        Search bar
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
