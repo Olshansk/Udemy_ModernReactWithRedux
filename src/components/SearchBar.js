a628e9db6ead5151abf660ecfc83dbbe821023ba
Daniel Olshansky <olshansky.daniel@gmail.com>
2021-05-09 08:38:19 -0700

    Add onInputChange and onInputClick

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,25 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+
+  onInputClick(event) {
+    console.log("click");
+  }
+
   render() {
     return (
       <div>
         <form className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text"></input>
+            <input
+              type="text"
+              onChange={this.onInputChange}
+              onClick={this.onInputClick}
+            ></input>
           </div>
         </form>
       </div>
