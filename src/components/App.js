b75fde03d91ca80acf513d4e02173eb8e8e6abe6
Daniel Olshansky <olshansky.daniel@gmail.com>
2021-05-30 08:21:40 -0700

    Installed axios and nothing else

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,9 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import axios from "axios";
+// https://unsplash.com/documentation
+// const ACCESS_KEY = '5OPUL1VkqKzHNB1ToeNF7CymkvY2cWI9Rj79AZqdQkc';
+// const SECRET_KEY = '2hpjIaz4sAuRoeEpYca-QOmpKW3junejkCtBdbsfv0o';
 
 class App extends React.Component {
   onSearchSubmit(term) {
