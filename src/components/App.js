ed272a1c47cd0c7fd1c93b807c35d571f711d9c9
Daniel Olshansky <olshansky.daniel@gmail.com>
2021-06-02 09:08:20 -0700

    Retrieving number of images available

--- src/components/App.js
+++ src/components/App.js
@@ -7,20 +7,27 @@ const ACCESS_KEY = "5OPUL1VkqKzHNB1ToeNF7CymkvY2cWI9Rj79AZqdQkc";
 // const SECRET_KEY = '2hpjIaz4sAuRoeEpYca-QOmpKW3junejkCtBdbsfv0o';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
+  state = { images: [] };
+
+  onSearchSubmit = async (term) => {
     console.log("onSearchSubmit: ", term);
-    axios.get(`https://api.unsplash.com/search/photos`, {
+    const response = await axios.get(`https://api.unsplash.com/search/photos`, {
       params: { query: term },
       headers: {
         Authorization: `Client-ID ${ACCESS_KEY}`,
       },
     });
-  }
+
+    this.setState({
+      images: response.data.results,
+    });
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found {this.state.images.length} images
       </div>
     );
   }
