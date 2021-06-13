79ab20f653790073f8c3e0b5ce01c6940d4f1c55
Daniel Olshansky <olshansky.daniel@gmail.com>
2021-06-12 21:01:33 -0700

    Add ImageCard element

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./ImageList.css";
+import ImageCard from "./ImageCard.js";
 
 class ImageList extends React.Component {
   render() {
     const images = this.props.image_data || [];
-    const image_elements = images.map((elem) => {
-      const { alt_description, id, urls } = elem;
-      return <img key={id} alt={alt_description} src={urls.full}></img>;
+    const image_elements = images.map((image) => {
+      return <ImageCard key={image.id} image={image}></ImageCard>;
     });
     return <div className="image-list">{image_elements}</div>;
   }
