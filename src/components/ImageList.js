aa04226cd39062091f3f32d0436e23bdea2d925d
Daniel Olshansky <olshansky.daniel@gmail.com>
2021-06-12 20:39:13 -0700

    Add ImageList.css

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,21 +1,14 @@
 import React from "react";
+import "./ImageList.css";
 
 class ImageList extends React.Component {
   render() {
     const images = this.props.image_data || [];
     const image_elements = images.map((elem) => {
       const { alt_description, id, urls } = elem;
-      return (
-        <img
-          key={id}
-          alt={alt_description}
-          src={urls.full}
-          width="400px"
-          height="400px"
-        ></img>
-      );
+      return <img key={id} alt={alt_description} src={urls.full}></img>;
     });
-    return <div>{image_elements}</div>;
+    return <div className="image-list">{image_elements}</div>;
   }
 }
 
