b5b7db08e98460551c57eefe7a8136cc1327485a
Daniel Olshansky <olshansky.daniel@gmail.com>
2021-06-17 08:45:57 -0700

    Stylized the search bar using semantic-ui

--- video_app_section_11/src/components/SearchBar.js
+++ video_app_section_11/src/components/SearchBar.js
@@ -1,8 +1,36 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = { term: "" };
+
+  onInputChange = (event) => {
+    this.setState({
+      term: event.target.value,
+    });
+  };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    // TODO: use axios to make a network request
+  };
+
   render() {
-    return <div>I am a Search Bar </div>;
+    return (
+      <div className="ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="ui field">
+            <label> Search Video</label>
+            <input
+              type="text"
+              name="video-search"
+              value={this.state.term}
+              placeholder="Enter query for video search"
+              onChange={this.onInputChange}
+            ></input>
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
