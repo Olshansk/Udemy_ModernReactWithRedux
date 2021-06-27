146b0ce53a9b7ca9ae665daefe32f0ed2d523507
Daniel Olshansky <olshansky.daniel@gmail.com>
2021-06-27 09:48:36 -0700

    Expand and collapse cells

--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -3,31 +3,27 @@ import React, { useState } from "react";
 const Accordion = ({ items }) => {
   const [activeIndex, setActiveIndex] = useState(null);
 
-  const onTitleClicked = (index) => {
-    console.log("Index clicked", index);
+  const onTitleClick = (index) => {
     setActiveIndex(index);
   };
 
   const renderedItems = items.map((item, index) => {
+    const active = index === activeIndex ? "active" : "";
+
     return (
       <React.Fragment key={item.title}>
-        <div className="title active" onClick={() => onTitleClicked(index)}>
+        <div className={`title ${active}`} onClick={() => onTitleClick(index)}>
           <i className="dropdown icon"></i>
           {item.title}
         </div>
-        <div className="content active">
+        <div className={`content ${active}`}>
           <p>{item.content}</p>
         </div>
       </React.Fragment>
     );
   });
-  return (
-    <div>
-      {" "}
-      <div className="ui styled accordion">{renderedItems}</div>
-      <h1>{activeIndex}</h1>
-    </div>
-  );
+
+  return <div className="ui styled accordion">{renderedItems}</div>;
 };
 
 export default Accordion;
