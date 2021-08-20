042f1440aaa126ef2f7d4fa12809414a4256d75a
Daniel Olshansky <olshansky.daniel@gmail.com>
2021-08-20 09:38:14 -0400

    Refactored App from class to function component

--- videos_app_hooks/src/components/App.js
+++ videos_app_hooks/src/components/App.js
@@ -1,61 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
 import VideoDetail from "./VideoDetail";
 
-class App extends React.Component {
-  state = {
-    videos: [],
-    selected_video: null,
-  };
+const App = () => {
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
 
-  componentDidMount() {
-    this.onTermSubmit("buildings");
-  }
-
-  onTermSubmit = async (term) => {
-    console.log(term);
+  const onTermSubmit = async (term) => {
     const response = await youtube.get("/search", {
       params: {
         q: term,
       },
     });
 
-    this.setState({
-      videos: response.data.items,
-      selected_video: response.data.items[0],
-    });
+    setVideos(response.data.items);
+    setSelectedVideo(response.data.items[0]);
   };
 
-  onVideoSelect = (video) => {
-    console.log("App - onVideoSelect", video);
-    this.setState({
-      selected_video: video,
-    });
-  };
+  useEffect(() => {
+    onTermSubmit("buildings");
+  }, []);
 
-  render() {
-    return (
-      <div className="ui container">
-        <SearchBar onTermSubmit={this.onTermSubmit} />
-        <div className="ui grid">
-          <div className="ui row">
-            <div className="eleven wide column">
-              <VideoDetail video={this.state.selected_video} />
-            </div>
-            <div className="five wide column">
-              <VideoList
-                onVideoSelect={this.onVideoSelect}
-                videos={this.state.videos}
-              />
-            </div>
+  return (
+    <div className="ui container">
+      <SearchBar onTermSubmit={onTermSubmit} />
+      <div className="ui grid">
+        <div className="ui row">
+          <div className="eleven wide column">
+            <VideoDetail video={selectedVideo} />
+          </div>
+          <div className="five wide column">
+            <VideoList
+              onVideoSelect={(video) => setSelectedVideo(video)}
+              videos={videos}
+            />
           </div>
         </div>
-        Num videos: {this.state.videos.length}.
       </div>
-    );
-  }
-}
+      Num videos: {videos.length}.
+    </div>
+  );
+};
 
 export default App;
